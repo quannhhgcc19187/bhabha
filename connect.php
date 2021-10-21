3aaab7b7ce18ce292e35c72a3ccabcbfaa0f98f7
quannhhgcc19187 <91667696+quannhhgcc19187@users.noreply.github.com>
2021-10-21 14:12:52 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
 	
 	mysqli_query($Connect,'SET NAMES "utf8"');
 	//mysqli_close($Connect);*/
-	$Connect = pg_connect("postgres://jmdrhuyxeteupz:59d17582c5cf3f93d4c70701169649492bf1ca1c26e7be4c1fed36ee1c91e358@ec2-34-205-14-168.compute-1.amazonaws.com:5432/d2hk0ut4ifpeo7");
+	$Connect = pg_connect("postgres://ordsisfwyvenwi:ce17e91e2c56f6e04bfb1cd5d26e92d6aa9afa212847cb23eaf55dc71622ea13@ec2-52-0-93-3.compute-1.amazonaws.com:5432/df6eulf6tnsrso");
     //$Connect = pg_connect("host=localhost port=5432 dbname=postgres");
 	//$Connect = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=123456");
 	
